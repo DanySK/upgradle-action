f2a133d3c3b62673504338bbb7bf61dd545f3aa5
Danilo Pianini <danilo.pianini@gmail.com>
2020-11-13 15:51:39 +0100

    First tentative version

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export GITHUB_TOKEN=$1
+CONFIG_FILE=$2
+VERSION=$3
+set -e
+if [ -z $VERSION ]
+then
+    VERSION=$(curl https://github.com/DanySK/upgradle/releases/latest | grep -Po 'href="https://github\.com/DanySK/upgradle/releases/tag/(\K.+)(?=")')
+fi
+curl -sL https://github.com/DanySK/upgradle/releases/download/$VERSION/upgradle-$VERSION-all.jar --output upgradle.jar
+java -jar upgradle.jar $CONFIG_FILE
+set +e
