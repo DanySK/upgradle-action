b98d059641c83d51b5245e43704c70608da101c7
Danilo Pianini <danilo.pianini@gmail.com>
2020-11-13 15:54:43 +0100

    Print the selected upgradle version

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ if [ -z $VERSION ]
 then
     VERSION=$(curl https://github.com/DanySK/upgradle/releases/latest | grep -Po 'href="https://github\.com/DanySK/upgradle/releases/tag/(\K.+)(?=")')
 fi
+echo "Selected UpGradle version $VERSION"
 curl -sL https://github.com/DanySK/upgradle/releases/download/$VERSION/upgradle-$VERSION-all.jar --output upgradle.jar
 java -jar upgradle.jar $CONFIG_FILE
 set +e
